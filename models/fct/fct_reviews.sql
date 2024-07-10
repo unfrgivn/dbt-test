cefdf31e3cdc44e4265cbb1f46a44fbd73dc9ea5
unfrgivn <bradley.ash@gmail.com>
2024-07-10 10:32:29 -0500

    updates

--- models/fct/fct_reviews.sql
+++ models/fct/fct_reviews.sql
@@ -1,24 +1,21 @@
-{{
-  config(
-    materialized = 'incremental',
-    on_schema_change='fail'
-    )
-}}
+{{ config(materialized="incremental", on_schema_change="fail") }}
 
-WITH src_reviews AS (
-  SELECT * FROM {{ ref('src_reviews') }}
-)
-SELECT 
-  {# {{ dbt_utils.generate_surrogate_key(['listing_id', 'review_date', 'reviewer_name', 'review_text']) }} as review_id, #}
-  *
-FROM src_reviews
-WHERE review_text is not null
+with src_reviews as (select * from {{ ref("src_reviews") }})
+select
+    {{
+        dbt_utils.generate_surrogate_key(
+            ["listing_id", "review_date", "reviewer_name", "review_text"]
+        )
+    }} as review_id, *
+from src_reviews
+where
+    review_text is not null
 
-{% if is_incremental() %}
-  AND review_date > (select max(review_date) from {{ this }})
-{% endif %}
+    {% if is_incremental() %}
+        and review_date > (select max(review_date) from {{ this }})
+    {% endif %}
 
-{# {% if is_incremental() %}
+    {# {% if is_incremental() %}
   {% if var("start_date", False) and var("end_date", False) %}
     {{ log('Loading ' ~ this ~ ' incrementally (start_date: ' ~ var("start_date") ~ ', end_date: ' ~ var("end_date") ~ ')', info=True) }}
     AND review_date >= '{{ var("start_date") }}'
@@ -28,3 +25,4 @@ WHERE review_text is not null
     {{ log('Loading ' ~ this ~ ' incrementally (all missing dates)', info=True)}}
   {% endif %}
 {% endif %} #}
+    
